4f3c558ab942c32a327cd2f753d230a05b4c20ef
LeadDev <leaddev@anon.farm>
2022-01-21 15:08:25 +0530

    update deployments

--- src/typechain-types/factories/Account__factory.ts
+++ src/typechain-types/factories/Account__factory.ts
@@ -205,7 +205,7 @@ const _abi = [
     anonymous: false,
     inputs: [
       {
-        indexed: false,
+        indexed: true,
         internalType: 'address',
         name: 'ownerAddress',
         type: 'address',
