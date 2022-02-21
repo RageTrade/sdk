a7d3a3e0c74c0f027a76821215fd0de60bdc8579
LeadDev <leaddev@anon.farm>
2022-02-21 21:44:08 +0530

    add default pool test

---
+++ test/default-pools.test.ts
@@ -0,0 +1,21 @@
+import { ethers } from 'ethers';
+import { getDefaultPoolContracts } from '../src/';
+
+import { config } from 'dotenv';
+config();
+
+describe('default pools', () => {
+  it('works', async () => {
+    const provider = new ethers.providers.StaticJsonRpcProvider(
+      'https://arb-rinkeby.g.alchemy.com/v2/' + process.env.ALCHEMY_KEY
+    );
+    const { vToken, vPool } = await getDefaultPoolContracts(provider);
+    if (vToken && vPool) {
+      expect(ethers.utils.isHexString(vToken.address)).toBeTruthy();
+      expect(ethers.utils.isHexString(vPool.address)).toBeTruthy();
+    } else {
+      // vTokena or vPool is undefined
+      expect(false).toBeTruthy();
+    }
+  });
+});
