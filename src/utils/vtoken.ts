59895db026cf024c9a9c7a88a82bd56aa291652b
fr0ntenddev <fr0ntenddev@rage.trade>
2022-03-12 13:34:47 +0530

    fix: new truncate function to handle 0 starting number edge case

--- src/utils/vtoken.ts
+++ src/utils/vtoken.ts
@@ -1,3 +1,9 @@
 export function truncate(address: string) {
-  return '0x' + address.slice(34, 42);
+  let temp = address.slice(34, 42);
+
+  while (temp[0] === '0' && temp.length > 1) {
+    temp = temp.slice(1);
+  }
+
+  return '0x' + temp;
 }
