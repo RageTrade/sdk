a70806bc9a8e5c736d4d3dc563dba8bccf2742e4
LeadDev <leaddev@anon.farm>
2022-03-24 21:11:16 +0530

    Fix slippage method

--- src/utils/slippage.ts
+++ src/utils/slippage.ts
@@ -2,7 +2,7 @@ import { BigNumber, BigNumberish } from 'ethers';
 import { toQ96, Q96 } from './fixed-point';
 
 /**
- *
+ * Calculate the sqrt price limit given the slippage.
  * @param slippage A fractional number between 0 and 1. 3% slippage is 0.03
  * @param sqrtPriceCurrent The current sqrt price of the token in the market
  * @returns
@@ -12,13 +12,11 @@ export function slippageToSqrtPriceLimit(
   sqrtPriceCurrent: BigNumberish
 ) {
   sqrtPriceCurrent = BigNumber.from(sqrtPriceCurrent);
-  const slippageX96 = toQ96(slippage);
+  const slippagePlusX96 = toQ96(Math.sqrt(1 + slippage));
+  const slippageMinusX96 = toQ96(Math.sqrt(1 - slippage));
+
   return {
-    sqrtPriceLimitX96ForBuy: sqrtPriceCurrent
-      .mul(Q96.add(slippageX96))
-      .div(Q96),
-    sqrtPriceLimitX96ForSell: sqrtPriceCurrent
-      .mul(Q96.sub(slippageX96))
-      .div(Q96),
+    sqrtPriceLimitX96ForBuy: sqrtPriceCurrent.mul(slippagePlusX96).div(Q96),
+    sqrtPriceLimitX96ForSell: sqrtPriceCurrent.mul(slippageMinusX96).div(Q96),
   };
 }
--- test/slippage.test.ts
+++ test/slippage.test.ts
@@ -1,13 +1,36 @@
-import { slippageToSqrtPriceLimit, Q96, fromQ96 } from '../src/';
+import { sqrtPriceX96ToPrice } from '../dist';
+import { slippageToSqrtPriceLimit } from '../src/';
 
 describe('slippage', () => {
-  it('works', () => {
+  it('works 2', async () => {
+    const sqrtPriceCurrent = '0x036b55100d8147fc7ac63a';
+    const priceCurrent = await sqrtPriceX96ToPrice(sqrtPriceCurrent, 6, 18);
+    const slippage = 0.001;
     const {
       sqrtPriceLimitX96ForBuy,
       sqrtPriceLimitX96ForSell,
-    } = slippageToSqrtPriceLimit(0.01, Q96.mul(3000));
+    } = slippageToSqrtPriceLimit(slippage, sqrtPriceCurrent);
 
-    expect(fromQ96(sqrtPriceLimitX96ForBuy, 2)).toEqual(3030);
-    expect(fromQ96(sqrtPriceLimitX96ForSell, 2)).toEqual(2970);
+    const priceLimitBuy = await sqrtPriceX96ToPrice(
+      sqrtPriceLimitX96ForBuy,
+      6,
+      18
+    );
+    const priceLimitSell = await sqrtPriceX96ToPrice(
+      sqrtPriceLimitX96ForSell,
+      6,
+      18
+    );
+
+    expect(
+      Math.round(
+        (Math.abs(priceLimitBuy - priceCurrent) / priceCurrent) * 1000
+      ) / 1000
+    ).toEqual(slippage);
+    expect(
+      Math.round(
+        (Math.abs(priceLimitSell - priceCurrent) / priceCurrent) * 1000
+      ) / 1000
+    ).toEqual(slippage);
   });
 });
