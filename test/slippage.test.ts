cc4251a58bb22f8e9c562cc1710a1a7c15a20fda
LeadDev <leaddev@anon.farm>
2022-06-17 20:50:06 +0530

    test: update slipage check

--- test/slippage.test.ts
+++ test/slippage.test.ts
@@ -31,4 +31,34 @@ describe('slippage', () => {
       ) / 1000
     ).toEqual(slippage);
   });
+
+  it('works 2', async () => {
+    const sqrtPriceCurrent = '0x036b55100d8147fc7ac63a';
+    const priceCurrent = await sqrtPriceX96ToPrice(sqrtPriceCurrent, 6, 18);
+    const slippage = 100;
+    const { sqrtPriceLimitX96ForBuy, sqrtPriceLimitX96ForSell } =
+      slippageToSqrtPriceLimit(slippage, sqrtPriceCurrent);
+
+    const priceLimitBuy = await sqrtPriceX96ToPrice(
+      sqrtPriceLimitX96ForBuy,
+      6,
+      18
+    );
+    const priceLimitSell = await sqrtPriceX96ToPrice(
+      sqrtPriceLimitX96ForSell,
+      6,
+      18
+    );
+
+    expect(
+      Math.round(
+        (Math.abs(priceLimitBuy - priceCurrent) / priceCurrent) * 1000
+      ) / 1000
+    ).toEqual(slippage);
+    expect(
+      Math.round(
+        (Math.abs(priceLimitSell - priceCurrent) / priceCurrent) * 1000
+      ) / 1000
+    ).toEqual(slippage);
+  });
 });
