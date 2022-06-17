dfb41c29c2b13af2e78b3c3e91379e142c930df0
LeadDev <leaddev@anon.farm>
2022-06-17 22:20:09 +0530

    Fix slippage logic

--- src/utils/slippage.ts
+++ src/utils/slippage.ts
@@ -1,5 +1,13 @@
+import { TickMath } from '@uniswap/v3-sdk';
 import { BigNumber, BigNumberish } from 'ethers';
 import { toQ96, Q96 } from './fixed-point';
+
+const MIN_SQRT_RATIO = BigNumber.from(
+  '0x' + TickMath.MIN_SQRT_RATIO.toString(16)
+);
+const MAX_SQRT_RATIO = BigNumber.from(
+  '0x' + TickMath.MAX_SQRT_RATIO.toString(16)
+);
 
 /**
  * Calculate the sqrt price limit given the slippage.
@@ -13,10 +21,18 @@ export function slippageToSqrtPriceLimit(
 ) {
   sqrtPriceCurrent = BigNumber.from(sqrtPriceCurrent);
   const slippagePlusX96 = toQ96(Math.sqrt(1 + slippage));
-  const slippageMinusX96 = toQ96(Math.sqrt(1 - slippage));
+  const slippageMinusX96 = toQ96(Math.sqrt(slippage < 1 ? 1 - slippage : 0));
 
-  return {
-    sqrtPriceLimitX96ForBuy: sqrtPriceCurrent.mul(slippagePlusX96).div(Q96),
-    sqrtPriceLimitX96ForSell: sqrtPriceCurrent.mul(slippageMinusX96).div(Q96),
-  };
+  let sqrtPriceLimitX96ForBuy = sqrtPriceCurrent.mul(slippagePlusX96).div(Q96);
+  if (sqrtPriceLimitX96ForBuy.gt(MAX_SQRT_RATIO)) {
+    sqrtPriceLimitX96ForBuy = MAX_SQRT_RATIO;
+  }
+
+  let sqrtPriceLimitX96ForSell = sqrtPriceCurrent
+    .mul(slippageMinusX96)
+    .div(Q96);
+  if (sqrtPriceLimitX96ForSell.lt(MIN_SQRT_RATIO)) {
+    sqrtPriceLimitX96ForSell = MIN_SQRT_RATIO;
+  }
+  return { sqrtPriceLimitX96ForBuy, sqrtPriceLimitX96ForSell };
 }
--- test/slippage.test.ts
+++ test/slippage.test.ts
@@ -1,8 +1,14 @@
+import { TickMath } from '@uniswap/v3-sdk';
+import { BigNumber } from 'ethers';
 import { sqrtPriceX96ToPrice } from '../dist';
 import { slippageToSqrtPriceLimit } from '../src/';
 
+const MIN_SQRT_RATIO = BigNumber.from(
+  '0x' + TickMath.MIN_SQRT_RATIO.toString(16)
+);
+
 describe('slippage', () => {
-  it('works 2', async () => {
+  it('works for small slippage', async () => {
     const sqrtPriceCurrent = '0x036b55100d8147fc7ac63a';
     const priceCurrent = await sqrtPriceX96ToPrice(sqrtPriceCurrent, 6, 18);
     const slippage = 0.001;
@@ -32,7 +38,7 @@ describe('slippage', () => {
     ).toEqual(slippage);
   });
 
-  it('works 2', async () => {
+  it('works for huge slippage', async () => {
     const sqrtPriceCurrent = '0x036b55100d8147fc7ac63a';
     const priceCurrent = await sqrtPriceX96ToPrice(sqrtPriceCurrent, 6, 18);
     const slippage = 100;
@@ -44,21 +50,12 @@ describe('slippage', () => {
       6,
       18
     );
-    const priceLimitSell = await sqrtPriceX96ToPrice(
-      sqrtPriceLimitX96ForSell,
-      6,
-      18
-    );
 
     expect(
       Math.round(
         (Math.abs(priceLimitBuy - priceCurrent) / priceCurrent) * 1000
       ) / 1000
     ).toEqual(slippage);
-    expect(
-      Math.round(
-        (Math.abs(priceLimitSell - priceCurrent) / priceCurrent) * 1000
-      ) / 1000
-    ).toEqual(slippage);
+    expect(sqrtPriceLimitX96ForSell).toEqual(MIN_SQRT_RATIO);
   });
 });
