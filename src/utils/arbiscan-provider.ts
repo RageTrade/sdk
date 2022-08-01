bce3725633a2a4abb4d1ecd982122fd85812bf4c
LeadDev <leaddev@anon.farm>
2022-08-01 20:30:07 +0530

    Add support for more topics in get logs arbiscan provider

--- src/utils/arbiscan-provider.ts
+++ src/utils/arbiscan-provider.ts
@@ -1,4 +1,5 @@
 import { ethers, logger } from 'ethers';
+import { Logger } from 'ethers/lib/utils';
 
 export class ArbiscanProvider extends ethers.providers.EtherscanProvider {
   constructor(network: 'arbmain' | 'arbtest', apiKey?: string) {
@@ -35,4 +36,110 @@ export class ArbiscanProvider extends ethers.providers.EtherscanProvider {
       this.network.name
     );
   }
+
+  async perform(method: string, params: any): Promise<any> {
+    switch (method) {
+      case 'getLogs': {
+        const args: Record<string, any> = { action: 'getLogs' };
+
+        if (params.filter.fromBlock) {
+          args.fromBlock = checkLogTag(params.filter.fromBlock);
+        }
+
+        if (params.filter.toBlock) {
+          args.toBlock = checkLogTag(params.filter.toBlock);
+        }
+
+        if (params.filter.address) {
+          args.address = params.filter.address;
+        }
+
+        // @TODO: We can handle slightly more complicated logs using the logs API
+        if (params.filter.topics && params.filter.topics.length > 0) {
+          const topic0 = params.filter.topics[0];
+          const topic1 = params.filter.topics[1];
+          const topic2 = params.filter.topics[2];
+          const topic3 = params.filter.topics[3];
+          if (topic0 !== undefined) {
+            if (typeof topic0 !== 'string' || topic0.length !== 66) {
+              logger.throwError(
+                'unsupported topic format',
+                Logger.errors.UNSUPPORTED_OPERATION,
+                { topic0 }
+              );
+            }
+            args.topic0 = topic0;
+          }
+          if (topic1 !== undefined) {
+            if (typeof topic1 !== 'string' || topic1.length !== 66) {
+              logger.throwError(
+                'unsupported topic format',
+                Logger.errors.UNSUPPORTED_OPERATION,
+                { topic1 }
+              );
+            }
+            args.topic1 = topic1;
+          }
+          if (topic2 !== undefined) {
+            if (typeof topic2 !== 'string' || topic2.length !== 66) {
+              logger.throwError(
+                'unsupported topic format',
+                Logger.errors.UNSUPPORTED_OPERATION,
+                { topic2 }
+              );
+            }
+            args.topic2 = topic2;
+          }
+          if (topic3 !== undefined) {
+            if (typeof topic3 !== 'string' || topic3.length !== 66) {
+              logger.throwError(
+                'unsupported topic format',
+                Logger.errors.UNSUPPORTED_OPERATION,
+                { topic3 }
+              );
+            }
+            args.topic3 = topic3;
+          }
+        }
+
+        const logs: Array<any> = await this.fetch('logs', args);
+
+        // Cache txHash => blockHash
+        let blocks: { [tag: string]: string } = {};
+
+        // Add any missing blockHash to the logs
+        for (let i = 0; i < logs.length; i++) {
+          const log = logs[i];
+          if (log.blockHash != null) {
+            continue;
+          }
+          if (blocks[log.blockNumber] == null) {
+            // This is commented to prevent unnecessary queries for block hash.
+            // const block = await this.getBlock(log.blockNumber);
+            // if (block) {
+            // blocks[log.blockNumber] = block.hash;
+            blocks[log.blockNumber] = ethers.constants.HashZero;
+            // }
+          }
+          log.blockHash = blocks[log.blockNumber];
+        }
+
+        return logs;
+      }
+      default:
+        return await super.perform(method, params);
+    }
+  }
+}
+
+// The blockTag was normalized as a string by the Provider pre-perform operations
+function checkLogTag(blockTag: string): number | 'latest' {
+  if (blockTag === 'pending') {
+    throw new Error('pending not supported');
+  }
+  if (blockTag === 'latest') {
+    return blockTag;
+  }
+
+  return parseInt(blockTag.substring(2), 16);
 }
--- test/arbiscan-provider.test.ts
+++ test/arbiscan-provider.test.ts
@@ -1,32 +1,66 @@
-import { ArbiscanProvider, getContracts, getPoolContracts } from '../dist';
+import {
+  ArbiscanProvider,
+  getContracts,
+  getPoolContracts,
+  getAccountIdsByAddress,
+} from '../dist';
 
 import { config } from 'dotenv';
 config();
 
 describe('arbiscan provider', () => {
-  it('arbmain', async () => {
-    const provider = new ArbiscanProvider('arbmain', process.env.ARBISCAN_KEY);
-    const bn = await provider.getBlockNumber();
-    expect(bn).toBeGreaterThan(0);
+  describe('arbmain network', () => {
+    it('getBlockNumber', async () => {
+      const provider = new ArbiscanProvider(
+        'arbmain',
+        process.env.ARBISCAN_KEY
+      );
+      const bn = await provider.getBlockNumber();
+      expect(bn).toBeGreaterThan(0);
+    });
+
+    it('getLogs', async () => {
+      const provider = new ArbiscanProvider(
+        'arbmain',
+        process.env.ARBISCAN_KEY
+      );
+      const { rageTradeFactory } = await getContracts(provider);
+      const pools = await getPoolContracts(rageTradeFactory);
+      expect(pools.length).toBeGreaterThan(0);
+    });
   });
 
-  it('arbmain getLogs', async () => {
-    const provider = new ArbiscanProvider('arbmain', process.env.ARBISCAN_KEY);
-    const { rageTradeFactory } = await getContracts(provider);
-    const pools = await getPoolContracts(rageTradeFactory);
-    expect(pools.length).toBeGreaterThan(0);
-  });
+  describe('arbtest network', () => {
+    it('getBlockNumber', async () => {
+      const provider = new ArbiscanProvider(
+        'arbtest',
+        process.env.ARBISCAN_KEY
+      );
+      const bn = await provider.getBlockNumber();
+      expect(bn).toBeGreaterThan(0);
+    });
 
-  it('arbtest', async () => {
-    const provider = new ArbiscanProvider('arbtest', process.env.ARBISCAN_KEY);
-    const bn = await provider.getBlockNumber();
-    expect(bn).toBeGreaterThan(0);
-  });
+    it('getLogs', async () => {
+      const provider = new ArbiscanProvider(
+        'arbtest',
+        process.env.ARBISCAN_KEY
+      );
+      const { rageTradeFactory } = await getContracts(provider);
+      const pools = await getPoolContracts(rageTradeFactory);
+      expect(pools.length).toBeGreaterThan(0);
+    });
 
-  it('arbmain getLogs', async () => {
-    const provider = new ArbiscanProvider('arbtest', process.env.ARBISCAN_KEY);
-    const { rageTradeFactory } = await getContracts(provider);
-    const pools = await getPoolContracts(rageTradeFactory);
-    expect(pools.length).toBeGreaterThan(0);
+    it('getAccountIdsByAddress', async () => {
+      const provider = new ArbiscanProvider(
+        'arbtest',
+        process.env.ARBISCAN_KEY
+      );
+      const { clearingHouse } = await getContracts(provider);
+      const accountIds = await getAccountIdsByAddress(
+        '0x5F326710bD6b9f3F525fdAa4241b37ee68984052',
+        clearingHouse
+      );
+      expect(accountIds.length).toBeGreaterThan(0);
+    });
   });
 });
