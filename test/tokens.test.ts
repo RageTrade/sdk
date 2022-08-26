846d11b7d5c4b7b67d2b2448b3232729d9324cfa
LeadDev <leaddev@anon.farm>
2022-08-26 12:22:43 +0530

    Add test case to ensure usdc is same in core and vaults

---
+++ test/tokens.test.ts
@@ -0,0 +1,33 @@
+import { ethers } from 'ethers';
+
+import { config } from 'dotenv';
+import { getContracts, getTokenContracts } from '../dist';
+
+config();
+
+describe('tokens', () => {
+  const providers: [string, ethers.providers.Provider][] = [
+    [
+      'arbmain',
+      new ethers.providers.StaticJsonRpcProvider(
+        'https://arb1.arbitrum.io/rpc'
+      ),
+    ],
+    [
+      'arbtest',
+      new ethers.providers.StaticJsonRpcProvider(
+        'https://arb-rinkeby.g.alchemy.com/v2/' + process.env.ALCHEMY_KEY
+      ),
+    ],
+  ];
+
+  for (const [name, provider] of providers) {
+    describe(name, () => {
+      it('usdc is same', async () => {
+        const { settlementToken } = await getContracts(provider);
+        const { usdc } = await getTokenContracts(provider);
+        expect(settlementToken.address).toEqual(usdc.address);
+      });
+    });
+  }
+});
