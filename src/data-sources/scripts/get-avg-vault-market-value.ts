f3d0e33239daed12c7e4bd376fc880babfe1ccbb
LeadDev <leaddev@anon.farm>
2022-09-27 14:31:30 +0000

    Get getAvgVaultMarketValue for two hour delay instead of one

--- src/data-sources/scripts/get-avg-vault-market-value.ts
+++ src/data-sources/scripts/get-avg-vault-market-value.ts
@@ -15,16 +15,18 @@ export async function getAvgVaultMarketValue(
   let timestamp = Math.floor(Date.now() / 1000);
   let vmvSum = BigNumber.from(0);
 
-  for (let i = 0; i < 24; i++) {
+  const hourDelay = 2;
+  let i = 0;
+  for (; i < 24 / hourDelay; i++) {
     const blockNumber = await dataSource.getBlockByTimestamp(timestamp);
     const vmv = await vault.getVaultMarketValue({
       blockTag: blockNumber,
     });
     vmvSum = vmvSum.add(vmv);
-    timestamp -= 3600;
+    timestamp -= 3600 * hourDelay;
   }
 
-  const avgVaultMarketValueD6 = vmvSum.div(24);
+  const avgVaultMarketValueD6 = vmvSum.div(i);
   return {
     avgVaultMarketValueD6,
     avgVaultMarketValue: Number(formatUsdc(avgVaultMarketValueD6)),
