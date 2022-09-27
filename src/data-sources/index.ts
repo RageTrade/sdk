e0801237761eb30315696821675ef3e9ba256432
LeadDev <leaddev@anon.farm>
2022-09-27 13:11:47 +0000

    Export scripts in scripts namespace

--- src/data-sources/index.ts
+++ src/data-sources/index.ts
@@ -45,3 +45,4 @@ export * from './fallback-data-source';
 export * from './ethers-provider-data-source';
 export * from './cache-server-data-source';
 export * from './scripts';
+export * as scripts from './scripts';
