dd32aaa864d51e177cd71a9a7452264e404019fc
LeadDev <leaddev@anon.farm>
2022-09-29 16:09:00 +0000

    Fix issues in getAccountInfo helper

--- src/utils/get-accounts.ts
+++ src/utils/get-accounts.ts
@@ -1,5 +1,9 @@
+import { BigNumber } from 'ethers';
 import { core, getNetworkName, NetworkName } from '../contracts';
-import { ClearingHouse, IClearingHouse } from '../typechain';
+import pools from '../pools.json';
+import { ClearingHouse, IClearingHouse, uniswapCore } from '../typechain';
+import { amountsForLiquidity } from './liquidity';
+import { tickToPrice } from './price-tick';
 
 export async function getAccountIdsByAddress(
   addr: string,
@@ -39,47 +43,79 @@ export async function getAccountInfo(
       clearingHouseLens
         .getAccountCollateralInfo(accountId, collateralId)
         .then((collateralInfo) => ({
-          collateralAddress: collateralInfo.balance,
+          collateralAddress: collateralInfo.collateral,
           collateralId,
           balance: collateralInfo.balance,
         }))
     )
   );
 
-  const activePools = activePoolIds.map(async (poolId) => {
-    const { balance, netTraderPosition, sumALastX128, activeTickRanges } =
-      await clearingHouseLens.getAccountPositionInfo(accountId, poolId);
+  const activePools = await Promise.all(
+    activePoolIds.map(async (poolId) => {
+      const { balance, netTraderPosition, sumALastX128, activeTickRanges } =
+        await clearingHouseLens.getAccountPositionInfo(accountId, poolId);
 
-    const liquidityPositions = await Promise.all(
-      activeTickRanges.map(async (tickRange) => {
-        const lpInfo = await clearingHouseLens.getAccountLiquidityPositionInfo(
-          accountId,
-          poolId,
-          tickRange.tickLower,
-          tickRange.tickUpper
-        );
-        return {
-          tickLower: tickRange.tickLower,
-          tickUpper: tickRange.tickUpper,
-          limitOrderType: lpInfo.limitOrderType,
-          liquidity: lpInfo.liquidity,
-          vTokenAmountIn: lpInfo.vTokenAmountIn,
-          sumALastX128: lpInfo.sumALastX128,
-          sumBInsideLastX128: lpInfo.sumBInsideLastX128,
-          sumFpInsideLastX128: lpInfo.sumFpInsideLastX128,
-          sumFeeInsideLastX128: lpInfo.sumFeeInsideLastX128,
-        };
-      })
-    );
+      let vPoolAddress = pools[networkName].find((pool) =>
+        BigNumber.from(pool.poolId).eq(poolId)
+      )?.vPoolAddress;
 
-    return {
-      poolId,
-      balance,
-      netTraderPosition,
-      sumALastX128,
-      liquidityPositions,
-    };
-  });
+      if (vPoolAddress === undefined) {
+        const poolInfo = await clearingHouseLens.getPoolInfo(poolId);
+        vPoolAddress = poolInfo.vPool;
+      }
+
+      const vPool = uniswapCore.UniswapV3Pool__factory.connect(
+        vPoolAddress,
+        clearingHouseLens.provider
+      );
+      const { sqrtPriceX96 } = await vPool.slot0();
+
+      const liquidityPositions = await Promise.all(
+        activeTickRanges.map(async (tickRange) => {
+          const lpInfo =
+            await clearingHouseLens.getAccountLiquidityPositionInfo(
+              accountId,
+              poolId,
+              tickRange.tickLower,
+              tickRange.tickUpper
+            );
+          const { vQuoteAmount, vTokenAmount } = amountsForLiquidity(
+            tickRange.tickLower,
+            sqrtPriceX96,
+            tickRange.tickUpper,
+            lpInfo.liquidity
+          );
+
+          // TODO take the decimals dynamically
+          const priceLower = await tickToPrice(tickRange.tickLower, 6, 18);
+          const priceUpper = await tickToPrice(tickRange.tickUpper, 6, 18);
+          return {
+            tickLower: tickRange.tickLower,
+            tickUpper: tickRange.tickUpper,
+            priceLower,
+            priceUpper,
+            limitOrderType: lpInfo.limitOrderType,
+            liquidity: lpInfo.liquidity,
+            vTokenAmountIn: lpInfo.vTokenAmountIn,
+            sumALastX128: lpInfo.sumALastX128,
+            sumBInsideLastX128: lpInfo.sumBInsideLastX128,
+            sumFpInsideLastX128: lpInfo.sumFpInsideLastX128,
+            sumFeeInsideLastX128: lpInfo.sumFeeInsideLastX128,
+            vQuoteAmount,
+            vTokenAmount,
+          };
+        })
+      );
+
+      return {
+        poolId,
+        balance,
+        netTraderPosition,
+        sumALastX128,
+        liquidityPositions,
+      };
+    })
+  );
 
   return {
     owner,
