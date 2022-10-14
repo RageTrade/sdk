aa0c2cca906b0fe8bbe97e62ce6a4886a1925662
LeadDev <leaddev@anon.farm>
2022-10-14 15:14:45 +0000

    Add dn vault to vaults

--- src/contracts/vaults.ts
+++ src/contracts/vaults.ts
@@ -1,43 +1,39 @@
 import { BaseVault__factory } from '../typechain';
+import { newError } from '../utils/loggers';
 import { NetworkName, SignerOrProvider } from './common';
+import { dnLbVault, gmxVault, tricryptoVault } from './protocols';
 
-export const tricryptoVaultMetaData: VaultMetadata = {
-  name: '80-20 TriCrypto Strategy',
-  assetName: 'TriCrypto Shares',
-};
+export const supportedVaultNames = [
+  'tricrypto',
+  'gmx',
+  'dn_gmx_senior',
+  'dn_gmx_junior',
+] as const;
 
-export const gmxVaultMetaData: VaultMetadata = {
-  name: '80-20 GLP Strategy',
-  assetName: 'GLP Shares',
-};
-export const dnGmxJuniorVaultMetaData: VaultMetadata = {
-  name: '80-20 DN GMX Junior Strategy',
-  assetName: 'GLP Shares',
-};
-
-export const dnGmxSeniorVaultMetaData: VaultMetadata = {
-  name: '80-20 DN GMX Senior Strategy',
-  assetName: 'GLP Shares',
-};
-
-export type VaultName =
-  | 'tricrypto'
-  | 'gmx'
-  | 'dn-gmx-senior'
-  | 'dn-gmx-junior'
-  | 'unknown';
-
-import { tricryptoVault, gmxVault } from './protocols';
+export type VaultName = typeof supportedVaultNames[number];
 
 export interface VaultMetadata {
   name: string;
   assetName: string;
 }
 
+export function getVaultName(vaultName: string): VaultName {
+  vaultName = vaultName.toLowerCase();
+  if (!supportedVaultNames.includes(vaultName.toLowerCase() as VaultName)) {
+    throw newError(
+      `vaultName ${vaultName} not recognized, supported: ${supportedVaultNames.join(
+        ', '
+      )}`
+    );
+  }
+  return vaultName as VaultName;
+}
+
 export async function getVault(
   signerOrProvider: SignerOrProvider,
   vaultName: VaultName
 ) {
+  vaultName = getVaultName(vaultName);
   const vaultSync = getVaultSync(vaultName);
   switch (vaultName) {
     case 'tricrypto':
@@ -62,36 +58,73 @@ export async function getVault(
           signerOrProvider
         ),
       };
+    case 'dn_gmx_senior':
+      const { dnGmxSeniorVault } = await dnLbVault.getContracts(
+        signerOrProvider
+      );
+      return {
+        ...vaultSync,
+        vault: BaseVault__factory.connect(
+          dnGmxSeniorVault.address,
+          signerOrProvider
+        ),
+      };
+    case 'dn_gmx_junior':
+      const { dnGmxJuniorVault } = await dnLbVault.getContracts(
+        signerOrProvider
+      );
+      return {
+        ...vaultSync,
+        vault: BaseVault__factory.connect(
+          dnGmxJuniorVault.address,
+          signerOrProvider
+        ),
+      };
     default:
-      throw new Error(`vaultName should be either tricrypto or gmx`);
+      throw newError(
+        `this should not happen: contracts/vaults/getVault(${vaultName})`
+      );
   }
 }
 
 export function getVaultSync(vaultName: VaultName) {
+  vaultName = getVaultName(vaultName);
   switch (vaultName) {
     case 'tricrypto':
       return {
-        metadata: tricryptoVaultMetaData,
+        metadata: {
+          name: '80-20 TriCrypto Strategy',
+          assetName: 'TriCrypto Shares',
+        },
         nativeProtocolName: 'CurveFinance',
       };
     case 'gmx':
       return {
-        metadata: gmxVaultMetaData,
+        metadata: {
+          name: '80-20 GLP Strategy',
+          assetName: 'GLP Shares',
+        },
         nativeProtocolName: 'GMX',
       };
-    case 'dn-gmx-junior':
+    case 'dn_gmx_junior':
       return {
-        metadata: dnGmxJuniorVaultMetaData,
+        metadata: {
+          name: '80-20 DN GMX Junior Strategy',
+          assetName: 'GLP Shares',
+        },
         nativeProtocolName: 'GMX',
       };
-    case 'dn-gmx-senior':
+    case 'dn_gmx_senior':
       return {
-        metadata: dnGmxSeniorVaultMetaData,
+        metadata: {
+          name: '80-20 DN GMX Senior Strategy',
+          assetName: 'GLP Shares',
+        },
         nativeProtocolName: 'GMX',
       };
     default:
-      throw new Error(
-        `vaultName should be either tricrypto, gmx, dn-gmx-junior or dn-gmx-senior`
+      throw newError(
+        `this should not happen: contracts/vaults/getVaultSync(${vaultName})`
       );
   }
 }
@@ -100,6 +133,7 @@ export function getVaultDeployBlockNumber(
   networkName: NetworkName,
   vaultName: VaultName
 ) {
+  vaultName = getVaultName(vaultName);
   switch (vaultName) {
     case 'tricrypto':
       return (
@@ -111,36 +145,13 @@ export function getVaultDeployBlockNumber(
         gmxVault.getDeployments(networkName).GMXYieldStrategyDeployment?.receipt
           ?.blockNumber || 0
       );
+    case 'dn_gmx_junior':
+    case 'dn_gmx_senior':
+      return (
+        dnLbVault.getDeployments(networkName).DnGmxJuniorVaultDeployment
+          ?.receipt?.blockNumber || 0
+      );
     default:
       return 0;
   }
 }
-
-// export async function getVaultAddressFromVaultName(
-//   provider: ethers.providers.Provider,
-//   vaultName: VaultName
-// ): Promise<string> {
-//   switch (vaultName) {
-//     case 'tricrypto':
-//       const { curveYieldStrategy } = await tricryptoVault.getContracts(
-//         provider
-//       );
-//       return curveYieldStrategy.address;
-//     case 'gmx':
-//       const { gmxYieldStrategy } = await gmxVault.getContracts(provider);
-//       return gmxYieldStrategy.address;
-//     default:
-//       throw new Error(`vaultName should be either tricrypto or gmx`);
-//   }
-// }
-
-// export function getNativeProtocolName(vaultName: VaultName) {
-//   switch (vaultName) {
-//     case 'tricrypto':
-//       return 'Curve';
-//     case 'gmx':
-//       return 'GMX';
-//     default:
-//       throw new Error(`vaultName should be either tricrypto or gmx`);
-//   }
-// }
---
+++ test/vaults.test.ts
@@ -0,0 +1,63 @@
+import {
+  getProvider,
+  sdk,
+  supportedVaultNames,
+  supportedNetworkNames,
+  aliasNetworkNames,
+} from '../dist';
+
+const skipIf: any = {
+  gmx: {
+    arbmain: true,
+  },
+  dn_gmx_senior: {
+    arbmain: true,
+    arbtest: true,
+    arbrinkeby: true,
+  },
+  dn_gmx_junior: {
+    arbmain: true,
+    arbtest: true,
+    arbrinkeby: true,
+  },
+};
+
+describe('vaults', () => {
+  for (const vaultName of supportedVaultNames) {
+    ((skipIf?.[vaultName as any] as any) === true ? describe.skip : describe)(
+      vaultName,
+      () => {
+        for (const networkName of supportedNetworkNames) {
+          if (aliasNetworkNames.includes(networkName)) continue;
+          ((skipIf?.[vaultName as any]?.[networkName as any] as any) === true
+            ? describe.skip
+            : describe)(networkName, () => {
+            it('getVaultName', async () => {
+              expect(sdk.getVaultName(vaultName)).toEqual(vaultName);
+            });
+            it('getVaultSync', async () => {
+              sdk.getVaultSync(vaultName);
+            });
+            it('getVault', async () => {
+              await sdk.getVault(getProvider(networkName), vaultName);
+            });
+            it('getVaultDeployBlockNumber', async () => {
+              const blockNumber = sdk.getVaultDeployBlockNumber(
+                networkName,
+                vaultName
+              );
+              expect(blockNumber).toBeGreaterThan(0);
+            });
+            it('getVaultDeployBlockNumber', async () => {
+              const blockNumber = sdk.getVaultDeployBlockNumber(
+                networkName,
+                vaultName
+              );
+              expect(blockNumber).toBeGreaterThan(0);
+            });
+          });
+        }
+      }
+    );
+  }
+});
