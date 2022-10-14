b441e2950997c22b772af918f62a95de934d9db6
LeadDev <leaddev@anon.farm>
2022-10-14 16:22:14 +0000

    Handle pool composition failure

--- src/data-sources/scripts/get-pool-composition.ts
+++ src/data-sources/scripts/get-pool-composition.ts
@@ -18,37 +18,56 @@ export async function getPoolComposition(
   provider: ethers.providers.Provider,
   vaultName: VaultName
 ): Promise<PoolCompositionResult> {
-  const { clearingHouse, eth_vToken } = await core.getContracts(provider);
+  try {
+    const { clearingHouse, eth_vToken } = await core.getContracts(provider);
 
-  const { vault } = await getVault(provider, vaultName);
+    const { vault } = await getVault(provider, vaultName);
 
-  const poolId = truncate(eth_vToken.address);
+    const poolId = truncate(eth_vToken.address);
 
-  // TODO
-  const vaultAccountId = await vault.rageAccountNo();
+    // TODO
+    const vaultAccountId = await vault.rageAccountNo();
 
-  // net position of eth * twap price
-  const netPosition = await clearingHouse.getAccountNetTokenPosition(
-    vaultAccountId,
-    poolId
-  );
-  const virtualPriceX128 = await clearingHouse.getVirtualTwapPriceX128(poolId);
+    // net position of eth * twap price
+    const netPosition = await clearingHouse.getAccountNetTokenPosition(
+      vaultAccountId,
+      poolId
+    );
+    const virtualPriceX128 = await clearingHouse.getVirtualTwapPriceX128(
+      poolId
+    );
 
-  const rageAmountD6 = netPosition.abs().mul(virtualPriceX128).div(Q128);
-  const nativeAmountD6 = (await vault.getVaultMarketValue()).sub(rageAmountD6);
+    const rageAmountD6 = netPosition.abs().mul(virtualPriceX128).div(Q128);
+    const nativeAmountD6 = (await vault.getVaultMarketValue()).sub(
+      rageAmountD6
+    );
 
-  const sum = nativeAmountD6.add(rageAmountD6);
-  const oneEth = parseEther('1');
+    const sum = nativeAmountD6.add(rageAmountD6);
+    const oneEth = parseEther('1');
 
-  return {
-    rageAmountD6,
-    nativeAmountD6,
-    rageAmount: Number(formatUsdc(rageAmountD6)),
-    nativeAmount: Number(formatUsdc(nativeAmountD6)),
+    return {
+      rageAmountD6,
+      nativeAmountD6,
+      rageAmount: Number(formatUsdc(rageAmountD6)),
+      nativeAmount: Number(formatUsdc(nativeAmountD6)),
 
-    ragePercentage: Number(formatEther(safeDiv(oneEth.mul(rageAmountD6), sum))),
-    nativePercentage: Number(
-      formatEther(safeDiv(oneEth.mul(nativeAmountD6), sum))
-    ),
-  };
+      ragePercentage: Number(
+        formatEther(safeDiv(oneEth.mul(rageAmountD6), sum))
+      ),
+      nativePercentage: Number(
+        formatEther(safeDiv(oneEth.mul(nativeAmountD6), sum))
+      ),
+    };
+  } catch {
+    const rageAmountD6 = BigNumber.from(0);
+    const nativeAmountD6 = BigNumber.from(0);
+    return {
+      rageAmountD6,
+      nativeAmountD6,
+      rageAmount: 0,
+      nativeAmount: 0,
+      ragePercentage: 0,
+      nativePercentage: 0,
+    };
+  }
 }
