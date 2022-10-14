7d8b8bdad03c3e56e984c38dc50e7eb3780da59b
LeadDev <leaddev@anon.farm>
2022-10-14 15:31:56 +0000

    Move default data source to different file

---
+++ src/data-sources/default-data-source.ts
@@ -0,0 +1,53 @@
+import { ethers } from 'ethers';
+import { getNetworkNameFromProvider, NetworkName } from '../contracts';
+import { BaseDataSource } from './base-data-source';
+import { CacheServerDataSource } from './cache-server-data-source';
+import { EthersProviderDataSource } from './ethers-provider-data-source';
+import {
+  FallbackDataSource,
+  FallbackDataSourceOptions,
+} from './fallback-data-source';
+
+export function getDefaultDataSourceSync(
+  networkName: NetworkName,
+  providerOrSource?:
+    | ethers.providers.Provider
+    | ethers.providers.Provider[]
+    | BaseDataSource
+    | BaseDataSource[],
+  fallbackDataSourceOptions?: FallbackDataSourceOptions
+) {
+  const cacheDS = new CacheServerDataSource(networkName);
+  const sourcesArray: BaseDataSource[] = [];
+
+  if (Array.isArray(providerOrSource)) {
+    for (const obj of providerOrSource) {
+      if (BaseDataSource.isDataSource(obj)) {
+        sourcesArray.push(obj);
+      } else if (ethers.providers.Provider.isProvider(obj)) {
+        sourcesArray.push(new EthersProviderDataSource(obj));
+      }
+    }
+  } else if (BaseDataSource.isDataSource(providerOrSource)) {
+    sourcesArray.push(providerOrSource);
+  } else if (ethers.providers.Provider.isProvider(providerOrSource)) {
+    sourcesArray.push(new EthersProviderDataSource(providerOrSource));
+  }
+
+  return new FallbackDataSource(
+    [cacheDS, ...sourcesArray],
+    fallbackDataSourceOptions
+  );
+}
+
+export async function getDefaultDataSource(
+  provider: ethers.providers.Provider,
+  fallbackDataSourceOptions?: FallbackDataSourceOptions
+) {
+  const networkName = await getNetworkNameFromProvider(provider);
+  return getDefaultDataSourceSync(
+    networkName,
+    provider,
+    fallbackDataSourceOptions
+  );
+}
--- src/data-sources/index.ts
+++ src/data-sources/index.ts
@@ -1,48 +1,7 @@
-import { ethers } from 'ethers';
-import { NetworkName } from '../contracts';
-import { BaseDataSource } from './base-data-source';
-import { CacheServerDataSource } from './cache-server-data-source';
-import { EthersProviderDataSource } from './ethers-provider-data-source';
-import {
-  FallbackDataSource,
-  FallbackDataSourceOptions,
-} from './fallback-data-source';
-
-export function getDefaultDataSourceSync(
-  networkName: NetworkName,
-  providerOrSource?:
-    | ethers.providers.Provider
-    | ethers.providers.Provider[]
-    | BaseDataSource
-    | BaseDataSource[],
-  fallbackDataSourceOptions?: FallbackDataSourceOptions
-) {
-  const cacheDS = new CacheServerDataSource(networkName);
-  const sourcesArray: BaseDataSource[] = [];
-
-  if (Array.isArray(providerOrSource)) {
-    for (const obj of providerOrSource) {
-      if (BaseDataSource.isDataSource(obj)) {
-        sourcesArray.push(obj);
-      } else if (ethers.providers.Provider.isProvider(obj)) {
-        sourcesArray.push(new EthersProviderDataSource(obj));
-      }
-    }
-  } else if (BaseDataSource.isDataSource(providerOrSource)) {
-    sourcesArray.push(providerOrSource);
-  } else if (ethers.providers.Provider.isProvider(providerOrSource)) {
-    sourcesArray.push(new EthersProviderDataSource(providerOrSource));
-  }
-
-  return new FallbackDataSource(
-    [cacheDS, ...sourcesArray],
-    fallbackDataSourceOptions
-  );
-}
-
 export * from './base-data-source';
 export * from './fallback-data-source';
 export * from './ethers-provider-data-source';
 export * from './cache-server-data-source';
+export * from './default-data-source';
 export * from './scripts';
 export * as scripts from './scripts';
